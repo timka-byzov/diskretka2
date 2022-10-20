42108af39c3331bf534f426b383513f42e4b4acc
timka-byzov <tb@lamart.com>
2022-10-20 17:18:16 +0500

    refactor task4

--- task4.py
+++ task4.py
@@ -12,14 +12,14 @@ p = get_primes_up_to_n(150)
 m = 1
 
 
-def f_recursive(iter, n, prime):
+def f_recursive(primes_included, perm_len, lcm):
     global m, p
-    for i in range(iter, len(p)):
+    for i in range(primes_included, len(p)):
         k = p[i]
-        while k <= n:
-            f_recursive(i + 1, n - k, k * prime)
+        while k <= perm_len:
+            f_recursive(i + 1, perm_len - k, k * lcm)
             k *= p[i]
-    m = max(m, prime)
+    m = max(m, lcm)
 
 
 n = int(input())
